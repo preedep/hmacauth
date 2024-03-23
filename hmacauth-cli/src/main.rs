e70180988fb82c8c524171cde17a5741cefe80f1
Preedee Ponchevin <preedee.digital@gmail.com>
2024-03-23 17:09:00 +0700

    - update cli project , add parameters

--- hmacauth-cli/src/main.rs
+++ hmacauth-cli/src/main.rs
@@ -1,4 +1,25 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    url: String,
+    /// Request ID
+    #[arg(short, long)]
+    request_id: String,
+    /// Message to send
+    #[arg(short, long)]
+    message: String,
+    /// Access key
+    #[arg(short, long)]
+    access_key: String,
+}
 #[tokio::main]
 async fn main() {
+    pretty_env_logger::init();
+    let args = Args::parse();
+
     println!("Hello, world!");
 }
