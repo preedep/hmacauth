3234be2272975b4115b16e58f16fa23c2374e2cc
Preedee Ponchevin <preedee.digital@gmail.com>
2024-03-23 10:29:04 +0700

    - add index.html

--- hmacauth-web/src/main.rs
+++ hmacauth-web/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpRequest, HttpServer};
+use log::info;
+use actix_files as fs;
+use actix_files::NamedFile;
+use actix_web::middleware::Logger;
+
+async fn index(_req: HttpRequest) -> actix_web::Result<NamedFile> {
+    Ok(NamedFile::open("./statics/index.html")?)
+}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    pretty_env_logger::init();
+    info!("Starting server at http://:8080");
+
+    HttpServer::new(move ||
+                        App::new()
+                            .wrap(Logger::default())
+                            .route("/", actix_web::web::get().to(index))
+    )
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
 }
